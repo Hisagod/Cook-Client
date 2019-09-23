ed845ed33f007570f0e430f9d00f2444382c27bb
HisGod <1160860077@qq.com>
2019-09-23 17:12:39 +0800

    首页加载Crash

--- app/src/main/java/com/aib/viewmodel/MainViewModel.java
+++ app/src/main/java/com/aib/viewmodel/MainViewModel.java
@@ -9,14 +9,14 @@ import com.aib.entity.TypeEntity;
 import com.aib.net.Resource;
 import com.aib.repository.MainRepository;
 
-import javax.inject.Inject;
 
 public class MainViewModel extends AndroidViewModel {
-    @Inject
-    protected MainRepository rep;
+
+    private MainRepository rep;
 
     public MainViewModel(@NonNull Application application) {
         super(application);
+        rep = new MainRepository();
     }
 
     public LiveData<Resource<TypeEntity>> getJson(String key) {
